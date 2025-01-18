7557ad9b16d8c88c6b2018824fb2925e5fd87a13
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-01-18 11:16:48 +0100

    feat: setup ts + vite working in dev and prod mode with onnxruntimeweb

--- vite-sam/src/App.tsx
+++ vite-sam/src/App.tsx
@@ -1,11 +1,324 @@
+import { useState, useEffect, useRef } from "react";
+import { cn } from "@/lib/utils";
 
-function App() {
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { LoaderCircle, Crop, ImageUp, Fan, Github } from "lucide-react";
+
+import {
+  resizeCanvas,
+  mergeMasks,
+  maskImageCanvas,
+  resizeAndPadBox,
+  canvasToFloat32Array,
+  sliceTensorMask,
+} from "@/lib/imageutils";
+
+export default function App() {
+  // resize+pad all images to 1024x1024
+  const imageSize = { w: 1024, h: 1024 };
+
+  // state
+  const [device, setDevice] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [imageEncoded, setImageEncoded] = useState(false);
+  const [status, setStatus] = useState("");
+
+  const samWorker = useRef(null);
+  const [image, setImage] = useState(null);
+  const [mask, setMask] = useState(null);
+  const [imageURL, setImageURL] = useState("./image_square.png");
+  const canvasEl = useRef(null);
+  const fileInputEl = useRef(null);
+
+  
+
+  // Start encoding image
+  const encodeImageClick = async () => {
+    samWorker.current.postMessage({
+      type: "encodeImage",
+      data: canvasToFloat32Array(resizeCanvas(image, imageSize)),
+    });
+
+    setLoading(true);
+    setStatus("Encoding");
+  };
+
+  // Start decoding, prompt with mouse coords
+  const imageClick = (event) => {
+    if (!imageEncoded) return;
+
+    const canvas = canvasEl.current;
+    const rect = event.target.getBoundingClientRect();
+
+    // input image will be resized to 1024x1024 -> normalize mouse pos to 1024x1024
+    const point = {
+      x: ((event.clientX - rect.left) / canvas.width) * imageSize.w,
+      y: ((event.clientY - rect.top) / canvas.height) * imageSize.h,
+      label: 1,
+    };
+
+    samWorker.current.postMessage({ type: "decodeMask", data: point });
+
+    setLoading(true);
+    setStatus("Decoding");
+  };
+
+  // Decoding finished -> parse result and update mask
+  const handleDecodingResults = (decodingResults) => {
+    // SAM2 returns 3 mask along with scores -> select best one
+    const maskTensors = decodingResults.masks;
+    const maskScores = decodingResults.iou_predictions.cpuData;
+    const bestMaskIdx = maskScores.indexOf(Math.max(...maskScores));
+    const maskCanvas = sliceTensorMask(maskTensors, bestMaskIdx);
+
+    setMask((prevMask) => {
+      if (prevMask) {
+        return mergeMasks(maskCanvas, prevMask);
+      } else {
+        return resizeCanvas(maskCanvas, imageSize);
+      }
+    });
+  };
+
+  // Handle web worker messages
+  const onWorkerMessage = (event) => {
+    const { type, data } = event.data;
+
+    if (type == "pong") {
+      const { success, device } = data;
+
+      if (success) {
+        setLoading(false);
+        setDevice(device);
+        setStatus("Encode image");
+      } else {
+        setStatus("Error (check JS console)");
+      }
+    } else if (type == "downloadInProgress" || type == "loadingInProgress") {
+      setLoading(true);
+      setStatus("Loading model");
+    } else if (type == "encodeImageDone") {
+      // alert(data.durationMs)
+      setImageEncoded(true);
+      setLoading(false);
+      setStatus("Ready. Click on image");
+    } else if (type == "decodeMaskResult") {
+      handleDecodingResults(data);
+      setLoading(false);
+      setStatus("Ready. Click on image");
+    }
+  };
+
+  // Crop image with mask
+  const cropClick = (event) => {
+    const link = document.createElement("a");
+    link.href = maskImageCanvas(image, mask).toDataURL();
+    link.download = "crop.png";
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  // Upload new image
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    const dataURL = window.URL.createObjectURL(file);
+
+    setImage(null);
+    setMask(null);
+    setImageEncoded(false);
+    setStatus("Encode image");
+    setImageURL(dataURL);
+  };
+
+  // Load web worker
+  useEffect(() => {
+    if (!samWorker.current) {
+      samWorker.current = new Worker(
+        new URL("./lib/worker.ts", import.meta.url),
+        { type: "module" }
+      );
+      samWorker.current.addEventListener("message", onWorkerMessage);
+      samWorker.current.postMessage({ type: "ping" });
+
+      setLoading(true);
+    }
+  }, [onWorkerMessage, handleDecodingResults]);
+
+  // Load image, pad to square and store in offscreen canvas
+  useEffect(() => {
+    if (imageURL) {
+      const img = new Image();
+      img.src = imageURL;
+      img.onload = function () {
+        const largestDim =
+          img.naturalWidth > img.naturalHeight
+            ? img.naturalWidth
+            : img.naturalHeight;
+        const box = resizeAndPadBox(
+          { h: img.naturalHeight, w: img.naturalWidth },
+          { h: largestDim, w: largestDim }
+        );
+
+        const canvas = document.createElement("canvas");
+        canvas.width = largestDim;
+        canvas.height = largestDim;
+
+        canvas
+          .getContext("2d")
+          .drawImage(
+            img,
+            0,
+            0,
+            img.naturalWidth,
+            img.naturalHeight,
+            box.x,
+            box.y,
+            box.w,
+            box.h
+          );
+        setImage(canvas);
+      };
+    }
+  }, [imageURL]);
+
+  // Offscreen canvas changed, draw it
+  useEffect(() => {
+    if (image) {
+      const canvas = canvasEl.current;
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(
+        image,
+        0,
+        0,
+        image.width,
+        image.height,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+    }
+  }, [image]);
+
+  // Mask changed, draw original image and mask on top with some alpha
+  useEffect(() => {
+    if (mask) {
+      const canvas = canvasEl.current;
+      const ctx = canvas.getContext("2d");
+
+      ctx.drawImage(
+        image,
+        0,
+        0,
+        image.width,
+        image.height,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+      ctx.globalAlpha = 0.4;
+      ctx.drawImage(
+        mask,
+        0,
+        0,
+        mask.width,
+        mask.height,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+      ctx.globalAlpha = 1;
+    }
+  }, [mask, image]);
 
   return (
-    <div className="p-10 h-screen w-screen">
-      VITE SAM
+    <div className="flex items-center justify-center min-h-screen bg-background p-4">
+      <Card className="w-full max-w-2xl">
+        <div className="absolute top-4 right-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() =>
+              window.open(
+                "https://github.com/giuseppeambrosio97/vite-sam",
+                "_blank"
+              )
+            }
+          >
+            <Github className="w-4 h-4 mr-2" />
+            View on GitHub
+          </Button>
+        </div>
+        <CardHeader>
+          <CardTitle>
+            <p>
+              Clientside Image Segmentation with onnxruntime-web and Meta's SAM2
+            </p>
+            <p
+              className={cn(
+                "flex gap-1 items-center",
+                device ? "visible" : "invisible"
+              )}
+            >
+              <Fan
+                color="#000"
+                className="w-6 h-6 animate-[spin_2.5s_linear_infinite] direction-reverse"
+              />
+              Running on {device}
+            </p>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-4">
+            <div className="flex justify-between gap-4">
+              <Button
+                onClick={encodeImageClick}
+                disabled={loading || imageEncoded}
+              >
+                <p className="flex items-center gap-2">
+                  {loading && <LoaderCircle className="animate-spin w-6 h-6" />}
+                  {status}
+                </p>
+              </Button>
+              {mask && (
+                <Button onClick={cropClick} variant="secondary">
+                  <Crop /> Crop
+                </Button>
+              )}
+              <Button
+                onClick={() => {
+                  fileInputEl.current.click();
+                }}
+                variant="secondary"
+                disabled={loading}
+              >
+                <ImageUp /> Change image
+              </Button>
+            </div>
+            <div className="flex justify-center">
+              <canvas
+                ref={canvasEl}
+                width={512}
+                height={512}
+                onClick={imageClick}
+              />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+      <input
+        ref={fileInputEl}
+        hidden
+        accept="image/*"
+        type="file"
+        onInput={handleFileUpload}
+      />
     </div>
-  )
+  );
 }
-
-export default App
--- vite-sam/vite.config.ts
+++ vite-sam/vite.config.ts
@@ -1,20 +1,61 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig, normalizePath } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  build: {
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      ...(isDev
+        ? [
+            viteStaticCopy({
+              targets: [
+                {
+                  src: normalizePath(
+                    path.resolve(__dirname, "./node_modules/onnxruntime-web/dist") +
+                      "/*.wasm"
+                  ),
+                  dest: "/node_modules/.vite/deps",
+                },
+              ],
+            }),
+          ]
+        : []),
+    ],
+    base: "./",
+    build: {
+      emptyOutDir: true,
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            onnx: ["onnxruntime-web"],
+          },
+        },
+      },
     },
-  },
-  server: {
-    port: 3000,
-  }
-})
+    worker: {
+      format: "es",
+      plugins: () => [tsconfigPaths()],
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "onnxruntime-web/all": path.resolve(
+          __dirname,
+          "node_modules/onnxruntime-web/dist/ort.all.bundle.min.mjs"
+        ),
+      },
+    },
+    server: {
+      port: 3000,
+    },
+    optimizeDeps: {
+      exclude: ["onnxruntime-web"],
+    },
+  };
+});
